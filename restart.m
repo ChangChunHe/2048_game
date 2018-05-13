8f2ff169991e14147f02765bc4932ae24875a113
hecc <33366949+ChangChunHe@users.noreply.github.com>
2018-05-13 13:09:02 +0800

    Add files via upload

--- restart.m
+++ restart.m
@@ -1,8 +1,9 @@
 function restart(~,~)
-global h start_tag
+global h start_tag h_patch
 if start_tag == 0
     start_tag = 1;
     delete(h(~isnan(h)))
+    delete(h_patch(~isnan(h_patch)))
     start
 end
 end
