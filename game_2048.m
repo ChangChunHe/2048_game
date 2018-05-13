5799afc217d662b224577fc33301da91066173a3
hecc <33366949+ChangChunHe@users.noreply.github.com>
2018-05-13 22:09:13 +0800

    modify some bugs when restarting a game

--- game_2048.m
+++ game_2048.m
@@ -1,4 +1,4 @@
-function main
+function game_2048
 clear
 close all
 clc
@@ -235,9 +235,10 @@ end
 
 
 function restart(~,~)
-global h start_tag h_patch
+global h start_tag h_patch  fail_tag
 if start_tag == 0
     start_tag = 1;
+    fail_tag = 0;
     delete(h(~isnan(h)))
     delete(h_patch(~isnan(h_patch)))
     start
