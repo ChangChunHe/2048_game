e77867b410b457297bae196e09b015589944dd97
hecc <33366949+ChangChunHe@users.noreply.github.com>
2018-07-05 19:15:53 +0800

    Delete next_right.m

--- ai_2048/next_right.m
+++
@@ -1,6 +0,0 @@
-function board_1 = next_right(board)
-board_1 = zeros(4);
-for k = 1:size(board,1)
-    tmp = fliplr(operation(fliplr(board(k,:))));
-    board_1(k,:) = [zeros(1, 4-length(tmp)) tmp];
-end
