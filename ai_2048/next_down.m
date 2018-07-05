24c0f6e3625f9a5b98af4a6dd9f92548b9e50668
hecc <33366949+ChangChunHe@users.noreply.github.com>
2018-07-05 19:15:38 +0800

    Delete next_down.m

--- ai_2048/next_down.m
+++
@@ -1,6 +0,0 @@
-function board_1 = next_down(board)
-board_1 = zeros(4);
-for k = 1:size(board,2)
-    tmp = flipud(operation(flipud(board(:, k)))')';
-    board_1(:, k) = [zeros(4-length(tmp), 1); tmp'];
-end
